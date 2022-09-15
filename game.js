95d4cd369c604cdae53fe1febfddea1fa6853838
Mike <you@example.com>
2022-09-15 16:49:11 -0500

    Random selection function for computer

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
 console.log("Hello");
+function getComputerChoice() {
+    const picked = Math.random()* 6 + 1;
+    console.log(picked)
+    if (picked <= 2) {
+        return ("Rock");
+    }
+    else if (picked >= 5) {
+        return ("Paper");
+    }
+    else {
+        return ("Scissors");
+    }
+    
+
+}
+
