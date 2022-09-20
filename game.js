9e0d9914ee99cd098adc35839b0d5547e5f29445
Mike <you@example.com>
2022-09-20 16:26:56 -0500

    Finished game. needs to loop now

--- game.js
+++ game.js
@@ -22,16 +22,37 @@ const playerSelection = prompt("Choose your weapon, Rock, Paper, or Scissors");
 console.log(playerSelection)
 function playRound() {
 
-    console.log(playerSelection)
+    
 if (playerSelection.toLowerCase() === "rock" && (computerSelection === "Paper")) {
     alert("Computer chose Paper! You Lose!");
     
 }
 else if (playerSelection.toLowerCase() === "rock" && (computerSelection === "Rock")) {
-    alert("Computer chose Rock! No winner!");
+    alert("Computer chose Rock! No Winner!");
 }
 else if (playerSelection.toLowerCase() === "rock" && (computerSelection === "Scissors")) {
     alert("Computer chose Scissors. You Win!");
 }
+else if (playerSelection.toLowerCase() === "paper" && (computerSelection === "Paper")) {
+    alert("Computer chose Paper! No Winner!");
+    
+}
+else if (playerSelection.toLowerCase() === "paper" && (computerSelection === "Rock")) {
+    alert("Computer chose Rock! You Win!");
+}
+else if (playerSelection.toLowerCase() === "paper" && (computerSelection === "Scissors")) {
+    alert("Computer chose Scissors. You Lose!");
+}
+else if (playerSelection.toLowerCase() === "scissors" && (computerSelection === "Paper")) {
+    alert("Computer chose Paper! You Win!");
+    
+}
+else if (playerSelection.toLowerCase() === "scissors" && (computerSelection === "Rock")) {
+    alert("Computer chose Rock! You Lose!");
+}
+else if (playerSelection.toLowerCase() === "scissors" && (computerSelection === "Scissors")) {
+    alert("Computer chose Scissors. No Winner!");
+}
+
 }
 playRound();
